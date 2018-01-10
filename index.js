fb7ab9d7f8ecf0c8999eeef7f8a81e3684cfb4f0
github username <juliannne@gmail.com>
2018-01-09 21:56:54 -0500

    Done.

--- index.js
+++ index.js
@@ -45,21 +45,10 @@ class Passenger {
 class Trip {
   constructor(driver, passenger) {
     this.id = ++id
-    if (driver) {
-      this.driverId = driver.id
-    }
-    if (passenger) {
-      this.passengerId = passenger.id
-    }
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    
     store.trips.push(this)
-  }
-
-  setDriver(driver) {
-    this.driverId = driver.id
-  }
-
-  setPassenger(passenger) {
-    this.passengerId = passenger.id
   }
 
   passenger() {
